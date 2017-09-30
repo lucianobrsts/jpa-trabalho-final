1bccdcf7af70c05a159020c6ccda4d42105fddd7
lucianobrsts <lucianobrsts@yahoo.com.br>
2017-09-30 20:39:42 -0300

    Finalizado projeto jpa...

---
+++ src/main/java/br/edu/uni7/persistence/DebitoTecnico.java
@@ -0,0 +1,45 @@
+package br.edu.uni7.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="TBL_DEBITOS_TECNICOS")
+@DiscriminatorValue("DT")
+@PrimaryKeyJoinColumn(name = "FK_ITEM_AVAL")
+public class DebitoTecnico extends ItemAvaliacao {
+	
+	@Column(name = "NU_CUSTO")
+	@Min(1)
+	@Max(5)
+	private Long custo;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	@Column(name = "ST_IMPACTO")
+	private Impacto impacto;
+
+	public Impacto getImpacto() {
+		return impacto;
+	}
+
+	public void setImpacto(Impacto impacto) {
+		this.impacto = impacto;
+	}
+
+	public Long getCusto() {
+		return custo;
+	}
+
+	public void setCusto(Long custo) {
+		this.custo = custo;
+	}
+}
---
+++ src/main/java/br/edu/uni7/persistence/Produto.java
@@ -0,0 +1,108 @@
+package br.edu.uni7.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "TBL_PRODUTOS")
+public class Produto {
+
+	@Id
+	@Column(name = "PK_PROD")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Column(name = "NM_NOME")
+	private String nome;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "FK_DEP")
+	@NotNull
+	private Departamento departamento;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "FK_USUARIO")
+	@NotNull
+	private Usuario responsavel;
+
+	@Enumerated
+	@Column(name = "ST_SITUACAO")
+	private Situacao situacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public Usuario getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(Usuario responsavel) {
+		this.responsavel = responsavel;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	public Situacao getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(Situacao situacao) {
+		this.situacao = situacao;
+	}
+
+}
